1167669b883e5725ff29edbe0ecee3cf8a60baed
seemewalkin <38620398+seemewalkin@users.noreply.github.com>
2024-06-03 15:01:26 +0200

    [main]: Fix spacing

--- app/src/androidTest/kotlin/com/programming/todolist/screens/AddEditScreen.kt
+++ app/src/androidTest/kotlin/com/programming/todolist/screens/AddEditScreen.kt
@@ -3,6 +3,7 @@ package com.programming.todolist.screens
 import androidx.compose.ui.test.junit4.ComposeTestRule
 import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.KNode
+
 class AddEditScreen(private val composeTestRule: ComposeTestRule) : ComposeScreen<AddEditScreen>(composeTestRule) {
     private val titleField = KNode(composeTestRule) { hasTestTag("TitleField") }
     private val descriptionField = KNode(composeTestRule) { hasTestTag("DescriptionField") }
